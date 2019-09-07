59a3600fde84be96c0e71d07409e009064276820
Vicente Brida <vicentebrida@outlook.com>
2019-09-07 14:32:08 -0300

    Finalized

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import Video from './components/Video';
+import Sidebar from './components/Sidebar';
+
+import store from './store';
 
 function App() {
   return (
     <div className="App">
-      
+      <Provider store={store}>
+        <Video />
+        <Sidebar />
+      </Provider>
     </div>
   );
 }
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,19 +1,35 @@
-import React, { Component } from "react";
+import React from 'react';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 // import { Container } from './styles';
 
-export default class Sidebar extends Component {
-  state = {
-    modules: [
-      {
-        id: 1,
-        title: "Iniciando com React",
-        lessons: [{}]
-      }
-    ]
-  };
+import * as CourseActions from '../../store/actions/course';
 
-  render() {
-    return <div />;
-  }
-}
+const Sidebar = ({ modules, toggleLesson }) => (
+  <aside>
+    { modules.map(module => (
+      <div key={module.id}>
+        <strong>{module.title}</strong>
+        <ul>
+          { module.lessons.map(lesson => (
+            <li key={lesson.id}>
+            {lesson.title}
+            <button onClick={() => toggleLesson(module, lesson)}>Selecionar</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </aside>
+);
+
+const mapStateToProps = state => ({
+  modules: state.course.modules
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CourseActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
