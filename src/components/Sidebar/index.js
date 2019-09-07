9f80af3d3633dbf754b6d6b5197562d05fc9fa44
Vicente Brida <vicentebrida@outlook.com>
2019-09-07 13:15:53 -0300

    Ajust

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+// import { Container } from './styles';
+
+export default class Sidebar extends Component {
+  state = {
+    modules: [
+      {
+        id: 1,
+        title: "Iniciando com React",
+        lessons: [{}]
+      }
+    ]
+  };
+
+  render() {
+    return <div />;
+  }
+}
